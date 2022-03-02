8d871d21a42abbc1098de0166224e1cd606d2632
ankit1470 <81144598+ankit1470@users.noreply.github.com>
2022-03-02 21:29:30 +0530

    Add files via upload

---
+++ Day-1/main.js
@@ -0,0 +1,10 @@
+const ad = require('./add')
+const sub = require('./subtract')
+const mul = require('./multiply')
+const div = require('./divide')
+var a=5
+var b=2
+console.log(ad(a,b));
+console.log(sub(a,b))
+console.log(mul(a,b))
+console.log(div(a,b))
